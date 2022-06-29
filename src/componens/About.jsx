c7567a0dd6d26ad088d065c542c3401e46bcb1e9
sheverda2005 <66365898+sheverda2005@users.noreply.github.com>
2022-06-29 08:08:47 +0300

    Чвбаб
    Кбкбк

--- src/componens/About.jsx
+++ src/componens/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <div className="container mt-3" >
-            <h1>Привіт, це сайт сворив Назар Шеверда</h1>
+            <h1>Привіт, цей сайт сворив Назар Шеверда</h1>
         </div>
     );
 }
